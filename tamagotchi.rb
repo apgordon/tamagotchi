db1b2a0fb97c0d3c8922a7961ecaf7b127859c07
apgordon <gordon.ap@gmail.com>
2017-09-30 13:47:34 -0400

    more class attribute and initial game loop

--- tamagotchi.rb
+++ tamagotchi.rb
@@ -1,21 +1,37 @@
 class Tamagotchi 
 
 	@@stages = ["Baby", "Child", "Teen", "Adult"]
-	attr_accessor :stage, :age, :weight 
+	attr_accessor :stage, :age, :weight, :hunger, :joy, :alive 
 
 	def initialize
 		@stage = @@stages[0]
 		@age = 0
 		@weight = rand(2..5)
+		@hunger = 1
+		@joy = 1
+		@alive = true 
 	end
 
 	def inspect
 		puts "Stage: #{@stage}"
-		puts "Age: #{@ago}"
-		puts "Weight: #{@weight}"
+		puts "Age: #{@age} yr"
+		puts "Weight: #{@weight} oz"
+		puts "Hunger: #{@hunger}/4"
+		puts "Joy #{@joy}/4"
+		puts "Alive: #{@alive} \n\n"
 	end
 
 end
 
 t = Tamagotchi.new
-t.inspect
+
+until t.alive == false 
+	print "Action? "
+	action = gets.chomp
+	puts " "
+
+	if action.downcase == 'inspect'
+		t.inspect
+	end
+
+end
